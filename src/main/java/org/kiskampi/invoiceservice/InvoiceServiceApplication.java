ec086523d720b81c697285272934873d1e56d19a
Ersan Ceylan <ersan@kodgemisi.com>
2019-02-10 14:19:03 +0300

    initial

---
+++ src/main/java/org/kiskampi/invoiceservice/InvoiceServiceApplication.java
@@ -0,0 +1,14 @@
+package org.kiskampi.invoiceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceServiceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/org/kiskampi/invoiceservice/InvoiceServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package org.kiskampi.invoiceservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
